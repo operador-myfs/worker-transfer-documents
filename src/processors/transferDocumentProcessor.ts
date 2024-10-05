12409bb5f74acd16c8c9698d986be39fe94990f8
Alejandro <realpealejo@gmail.com>
2024-10-05 03:44:39 -0500

    feat: Update transfer document processor

--- src/processors/transferDocumentProcessor.ts
+++ src/processors/transferDocumentProcessor.ts
@@ -7,6 +7,21 @@ import { getTransactionDoc } from '../helpers/getTransactionDoc';
 import { confirmTransfer } from '../helpers/confirmTransfer';
 
 export const transferDocumentProcessor = async (message: ITransferMessage): Promise<{ success: Boolean }> => {
-  console.log('Document transfered!', message);
-  return { success: true };
+  const key = `${Date.now()}-${message.key.trim().replaceAll(' ', '+')}`;
+
+  const { success: uploadSuccess } = await uploadFileToS3(message.id, message.url, key);
+  const { success: saveDocSuccess } = await saveDocToDB(message.id, message.key, key);
+
+  if (uploadSuccess == false || saveDocSuccess == false) {
+    await updateTransferTransaction('error', message.transactionId, message.key);
+    return { success: false };
+  }
+
+  const { success: updateDocSuccess } = await updateTransferTransaction('success', message.transactionId, message.key);
+  const { success: getDocSuccess, doc } = await getTransactionDoc(message.transactionId);
+
+  const isTransferCompleted = Object.entries(doc.documents).every(([_key, value]) => value.state === 'success');
+  if (isTransferCompleted) await confirmTransfer(message.id, doc.confirmationURL);
+
+  return { success: updateDocSuccess === true && getDocSuccess === true };
 };
